e4a46b99c1f8071127cbfc02b86de6e62035aa8d
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2025-03-15 22:28:02 +0530

    login con

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  
+  //Initial Route
+  {
+    path: '', redirectTo: '/welcome', pathMatch: 'full'
+  },
+
+  //Welcome Route
+  {
+    path: 'welcome', component: WelcomeComponent
+  },
+
+  //Login Route
+  {
+    path: 'login', component: LoginComponent
+  },
+
+  //Page Not Found Route (component: PageNotFound)
+  {
+    path: '**', redirectTo: '/welcome', pathMatch: 'full'
+  }
+
+
 ];
 
 @NgModule({
