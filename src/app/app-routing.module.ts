41e2805e75883d6a6bd1494e95951115ea2d2f1b
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2025-03-16 23:07:37 +0530

    fix:routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,33 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { AllDoctorsComponent } from './all-doctors/all-doctors.component';
 
 const routes: Routes = [
-  //Initial Route
   {
     path: '', redirectTo: '/welcome', pathMatch: 'full'
   },
-
-  //Welcome Route
   {
     path: 'welcome', component: WelcomeComponent
   },
-
-  //Login Route
   {
     path: 'login', component: LoginComponent
   },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'doctors', component: AllDoctorsComponent
+  }
 
   // //Page Not Found Route (component: PageNotFound)
   // {
